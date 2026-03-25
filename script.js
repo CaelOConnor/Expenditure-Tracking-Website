06efd45a1de358babf6af38d0ed43404087969c7
arthurvlw <arthurvlw4@gmail.com>
2026-03-25 15:59:05 -0400

    JSON
    Added JSON file with dummy data

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function showform(formId){
-    document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"));
-    document.getElementById(formId).classList.add("active");
+function showform(formId) {
+  document
+    .querySelectorAll(".form-box")
+    .forEach((form) => form.classList.remove("active"));
+  document.getElementById(formId).classList.add("active");
 }
