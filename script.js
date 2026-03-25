b2bd4e94b4c8190168685216fe8363e6c90251ab
CaelOConnor <coconnor10@students.stonehill.edu>
2026-03-25 16:06:53 -0400

    script.js fix

--- script.js
+++ script.js
@@ -2,8 +2,3 @@ function showform(formId){
     document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"));
     document.getElementById(formId).classList.add("active");
 }
-
-function showAddButtonPopup(formId){
-    document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"));
-    document.getElementById(formId).classList.add("active");
-}
