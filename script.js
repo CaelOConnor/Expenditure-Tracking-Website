4c67ed62ab1619b2f93574efd90b41923a1e5950
CaelOConnor <coconnor10@students.stonehill.edu>
2026-03-25 15:57:53 -0400

    Popup for adding expenditure
    doesn't connect to existing add button yest just makes it appear on screen

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function showform(formId){
     document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"));
     document.getElementById(formId).classList.add("active");
 }
+
+function showAddButtonPopup(formId){
+    document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"));
+    document.getElementById(formId).classList.add("active");
+}
