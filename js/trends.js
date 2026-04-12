61a0bef656cd4c06e8b3901fffce1d2331d83d54
CaelOConnor <coconnor10@students.stonehill.edu>
2026-04-12 14:48:35 -0400

    made trends.js
    Its just an outline for now

---
+++ js/trends.js
@@ -0,0 +1,38 @@
+class App {
+    constructor{
+        // call methods
+    }
+
+    // get data for charts
+    getMonthySpending(expenses){
+        // get monthly spending from past year and return the data for chart
+    }
+
+    getCategoryData(expenses){
+        // get amount spent for each category and return the data for chart
+    }
+
+    getTopExpenses(expenses){
+        // get top 10 expenses and return data for the chart
+    }
+
+
+    // render charts with vegalite
+    renderLineChart(data){  
+        // year to date spending line grpah
+
+    }
+
+    renderPieChart(data){
+        // pie chart of spending in different categorires
+
+    }
+
+    renderBarChart(data){
+        // bar chart of highest expenses
+
+    }
+    
+}
+
+export default App;
