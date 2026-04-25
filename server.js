99fb891e0436020739e0b77eb841d6cfc7ef0af0
CaelOConnor <coconnor10@students.stonehill.edu>
2026-04-25 14:55:33 -0400

    basic server setup and reorderd data.json

--- server.js
+++ server.js
@@ -1,26 +1,102 @@
 import express from 'express';
+import fs from 'fs';
+import data from './data.json' with {type: "json"};
 
 const app = express();
 
+app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/data', function(req, res){
-    //res.set("Access-Control-Allow-Origin", 'http://127.0.0.1:5500');
-    res.json([
-        {
-            "text": "What a lovely day!",
-            "profilePic": "images/tree-icon.png"
-        },
-        {
-            "text": "Hey, how ya doing today?",
-            "profilePic": "images/tree-icon.png"
-        },
-        {
-            "text": "Sup.",
-            "profilePic": "images/tree-icon.png"
-        }
-    ]);
+// when user logs in
+app.post('/login', function(req, res){
+    // hard coded user based on feedback
+    const user = data["arthur@gmail.com"];
+    // push data
+    user.push(req.body);
+
+    // save to file
+    fs.writeFile(
+        './data.json',
+        JSON.stringify(posts),
+        'utf-8',
+        function(err) {
+            if(err) console.log(err);
+            else console.log("Data Written successfully");
+    });
+    
 });
+
+// when user make new account
+app.post('/register', function(req, res){
+    // hard coded user based on feedback
+    const user = data["arthur@gmail.com"];
+    // push data
+    user.push(req.body);
+
+    // save to file
+    fs.writeFile(
+        './data.json',
+        JSON.stringify(posts),
+        'utf-8',
+        function(err) {
+            if(err) console.log(err);
+            else console.log("Data Written successfully");
+    });
+    
+});
+
+// get expenses for home page
+app.get('/expenses', function(req, res){
+    // hard coded user based on feedback
+    const user = data["arthur@gmail.com"];
+    // return expenses
+    res.json(user.expenses);
+});
+
+// createing a new expense on home page
+app.post('/expense', function(req, res){
+    // hard coded user based on feedback
+    const user = data["arthur@gmail.com"];
+    // push data
+    user.push(req.body);
+
+    // save to file
+    fs.writeFile(
+        './data.json',
+        JSON.stringify(posts),
+        'utf-8',
+        function(err) {
+            if(err) console.log(err);
+            else console.log("Data Written successfully");
+    });
+});
+
+// getting user limits
+app.get('/limit', function(req, res){
+    // hard coded user
+    const user = data["arthur@gmail.com"];
+    // return limits
+    res.json(user.limits)
+});
+
+// creating a limit
+app.post('/limit', function(req, res){
+    // hard coded user based on feedback
+    const user = data["arthur@gmail.com"];
+    // push data
+    user.push(req.body);
+
+    // save to file
+    fs.writeFile(
+        './data.json',
+        JSON.stringify(posts),
+        'utf-8',
+        function(err) {
+            if(err) console.log(err);
+            else console.log("Data Written successfully");
+    });
+});
+
 
 app.listen(3000, function(){
     console.log('Server listening on port 3000');
